e7b5730ad0ab9e3066b57084ae4b8f222fe3abeb
empanadacamilo <camilo.ramirez.2019@upb.edu.co>
2019-02-23 10:28:58 -0500

    "mi primera modificacion"

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-a=int(input("ingrese el primer numero", ))
-c=int(input("ingrese el segundo numero", ))
-multiplicacion=a*c
-print("el resultado es multiplicacion",multiplicacion)
-print("el doble del numero a", )
-doble=a*2
-print("el doble de a es",doble)
+b=int(input("digite el numero", ))
+cuadrado=b*b
+print("el resultado es ",cuadrado)
+d=float(input("digite el numero", ))
+print("el resultado de la raiz es: ", d**(1/2))
