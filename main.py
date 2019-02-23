ae1e31ed106d110b28f6b4d582d89407adee8615
empanadacamilo <camilo.ramirez.2019@upb.edu.co>
2019-02-23 11:02:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-b=int(input("digite el numero", ))
-cuadrado=b*b
-print("el resultado es ",cuadrado)
-d=float(input("digite el numero", ))
-print("el resultado de la raiz es: ", d**(1/2))
+a=float(input("ingrese el primer numero", ))
+b=float(input("ingrese el segundo numero", ))
+c=float(input("ingrese el tercer numero ", ))
+d=(((b**2)-(4*a*c))**(1/2))
+x1=(((-1*b)+(d**(1/2))))/2*a
+x2=(((-1*b)-(d**(1/2))))/2*a
+x3=(-1*b)/2*a
+if d>0:
+  print("los resultados ",x1 , x2, )
+else:
+  if d==0:
+    print("el resultado es",x3)
+  else:
+   print("el resutado no existe")
