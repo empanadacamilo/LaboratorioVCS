e78d4884955f52da9642c2511dc388205a411038
unknown <camilo.ramirez.2019@upb.edu.co>
2019-02-23 09:39:25 -0500

    mi primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LaboratorioVCS_camilogomez
---
+++ main.py
@@ -0,0 +1,7 @@
+a=int(input("ingrese el primer numero", ))
+c=int(input("ingrese el segundo numero", ))
+multiplicacion=a*c
+print("el resultado es multiplicacion",multiplicacion)
+print("el doble del numero a", )
+doble=a*2
+print("el doble de a es",doble)
